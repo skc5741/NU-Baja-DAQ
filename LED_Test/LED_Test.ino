1f4745779a1b2f6f61865b215010085167a914ed
skc5741 <skc5741@gmail.com>
2018-11-03 14:26:45 -0500

    Update LED_Test/LED_Test.ino

--- LED_Test/LED_Test.ino
+++ LED_Test/LED_Test.ino
@@ -1,18 +1,19 @@
-int LED = 13;
+int LED = 14;
 int timeDelay = 500;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
+  pinMode(13, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED, HIGH);
+  digitalWrite(13, HIGH);
   delay(timeDelay);
   digitalWrite(LED, LOW);
+  digitalWrite(13, LOW);
   delay(timeDelay);
-  
-
 }
